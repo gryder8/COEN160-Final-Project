6c804a7bc44f80bf2b5e1ef8efcad4229bcfb0e6
gryder8 <gavin.ryder82@gmail.com>
2022-10-14 23:11:46 -0700

    Got parse working, trimmed lib

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import com.fasterxml.jackson.databind.*;
+
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        WordDictionary dict = new WordDictionary();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            JsonNode node = mapper.readValue(new File("data/dictionary.json"), JsonNode.class);
+            for (char alphabet = 'a'; alphabet <= 'z'; alphabet++) {
+                String letter = String.valueOf(alphabet);
+                JsonNode array = node.get(letter);
+                String[] arr = new String[array.size()];
+                for (int i = 0; i < array.size(); i++) {
+                    String valToAdd = array.get(i).asText();
+                    arr[i] = valToAdd;
+                }
+                System.out.println("Found " + arr.length + " words for letter " + letter);
+                dict.data.put(letter, arr);
+            }
+            System.out.println("Found " + dict.data.keySet().size() + " letters in JSON");
+        } catch (Exception e) {
+            System.out.println("e = " + e);
+        }
     }
 }
